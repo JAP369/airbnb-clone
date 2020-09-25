0e17bf3fac2362fd02e01970f7aad929ea3fdddc
JonathanPaita <jonathan.austria.paita@gmail.com>
2020-09-26 02:16:19 +0800

    finalized sidebar, footer, videocard components and deployed to firebase

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import LanguageIcon from '@material-ui/icons/Language';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Avatar } from '@material-ui/core';
+
+function Header() {
+  return (
+    <div className='header'>
+      <img
+        src='https://insidetravel.news/wp-content/uploads/2018/10/airbnblogo.png'
+        alt=''
+        className='header__icon'
+      />
+      <div className='header__center'>
+        <input type='text' />
+        <SearchIcon />
+      </div>
+
+      <div className='header__right'>
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
