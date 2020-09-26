b0dcc9a65d8f342fa45efbfaac6f9d81f032917f
JonathanPaita <jonathan.austria.paita@gmail.com>
2020-09-26 22:59:27 +0800

    completed the clone

--- src/components/Banner/Search/Search.jsx
+++ src/components/Banner/Search/Search.jsx
@@ -5,9 +5,11 @@ import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
 import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
 //DATE PICKER COMPONENT
 function Search() {
+  const history = useHistory();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
@@ -30,7 +32,7 @@ function Search() {
         <PeopleAltIcon />
       </h2>
       <input type='number' min={0} defaultValue={2} />
-      <Button>Search Airbnb</Button>
+      <Button onClick={() => history.push('/search')}>Search Airbnb</Button>
     </div>
   );
 }
